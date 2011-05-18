905837138018934aa916711295288d0bd0a17753
Ryan Resella <ryan@codeforamerica.org>
2011-05-18 15:37:05 -0400

    Wrote spec for by_category

--- spec/sba_ruby/client_spec.rb
+++ spec/sba_ruby/client_spec.rb
@@ -4,4 +4,11 @@ describe SBA::Client do
   before do
     @client = SBA::Client.new
   end  
+  
+  describe "#by_category" do
+     it "should fetch licenses and permits by category" do
+       category = @client.by_category
+       category.first.title.should == "Register a Business Name"
+     end
+   end
 end
