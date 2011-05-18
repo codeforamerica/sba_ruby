a8af4dd400303aa597a2447fc73f0d26c6de2b19
William Dix <wjdix@northwestern.edu>
2011-05-18 16:01:26 -0400

    added spec for all urls method and implementation

--- lib/sba/client/urls.rb
+++ lib/sba/client/urls.rb
@@ -5,7 +5,10 @@ module SBA
   class Client
     module Urls
     include SBA::Client::Connection
-
+      def all_primary_urls_in_state(state, options={})
+        options.merge!({:format=> "json"})
+        response = get("geodata/primary_city_county_links_for_state_of/#{state}.json")
+      end
     end
   end
 end
--- spec/urls_spec.rb
+++ spec/urls_spec.rb
@@ -1,4 +1,30 @@
 require 'helper'
 
-describe SBA do
+describe SBA, ".primary_url" do
+
+  context "when one state" do
+    before do
+      stub_request(:get, 'http://api.sba.gov/geodata/primary_city_county_links_for_state_of/ks.json').
+        with().
+        to_return(:body => fixture('city_links_for_state.json'),
+                  :headers => {'Content-Type' => 'application/json'})
+    end
+    it "should request the correct resource" do
+      SBA.all_primary_urls_in_state('ks')
+      a_request(:get, 'http://api.sba.gov/geodata/primary_city_county_links_for_state_of/ks.json').
+        with().
+        should have_been_made
+    end
+    it "should return the correct results" do
+      test = SBA.all_primary_urls_in_state('ks')
+      test.should be_an Array
+      test[0]["url"].should == "http://www.abilenecityhall.com/"
+      test[1]["url"].should == "http://www.andoverks.com/"
+    end
+  end
+  context "when one city" do
+  end
+  context "when one county" do
+    
+  end
 end
