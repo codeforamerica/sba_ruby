f5a21964c971671b4e7f4035b2acf3a529c530c9
William Dix <wjdix@northwestern.edu>
2011-05-18 16:50:30 -0400

    implementation for city and county urls

--- lib/sba/client/urls.rb
+++ lib/sba/client/urls.rb
@@ -17,6 +17,14 @@ module SBA
         options.merge!({:format => "json"})
         response = get("geodata/primary_county_links_for_state_of/#{state}.json")
       end
+      def primary_url_for_city(city, state, options={})
+        options.merge!({:format => "json"})
+        response = get("geodata/primary_links_for_city_of/#{city}/#{state}.json")
+      end
+      def primary_url_for_county(county, state, options={})
+        options.merge!({:format => "json"})
+        response = get("geodata/primary_links_for_county_of/#{county}/#{state}.json")
+      end
     end
   end
 end
