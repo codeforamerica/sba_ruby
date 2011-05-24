a854e992da520919fdadc485ee2be88bca4eb5b9
Ryan Resella <ryan@codeforamerica.org>
2011-05-24 16:53:03 -0700

    Added loan_grants_by_federal

--- lib/sba/client/loans.rb
+++ lib/sba/client/loans.rb
@@ -5,6 +5,11 @@ module SBA
   class Client
     module Loans
     include SBA::Client::Connection
+    
+      def loan_grants_by_federal(options={})
+        options.merge!({:format=> "json"})
+        response = get("loans_grants/federal.json")
+      end
 
     end
   end
