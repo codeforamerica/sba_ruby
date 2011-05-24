312d69c3126f7d615c6acfb5b7ff65b6b8b9944b
Ryan Resella <ryan@codeforamerica.org>
2011-05-24 15:49:09 -0700

    Moved license methods out of client and into licenses.rb

--- lib/sba/client.rb
+++ lib/sba/client.rb
@@ -23,21 +23,6 @@ module SBA
     include SBA::Client::Licenses
     include SBA::Client::Sites  
     
-    # Returns results for a matching license or permit category for each 54 states and territories. 
-    #
-    # @param options [Hash] A customizable set of options.
-    # @option options category [String]  category corresponds to a requirement that generally applies to all businesses
-    # @return [Array<Hashie::Mash>]
-    # @example
-    #   @client = SBA.new
-    #   @client.by_category("doing business as")
-    def by_category(category, options={})
-      get("/license_permit/by_category/#{category}.json", options)
-    end
-    
-    def by_state(state, options={})
-      get("/license_permit/all_by_state/#{state}.json", options)
-    end
     
               
   end
--- lib/sba/client/licenses.rb
+++ lib/sba/client/licenses.rb
@@ -5,7 +5,21 @@ module SBA
   class Client
     module Licenses
     include SBA::Client::Connection
-
+      # Returns results for a matching license or permit category for each 54 states and territories. 
+      #
+      # @param options [Hash] A customizable set of options.
+      # @option options category [String]  category corresponds to a requirement that generally applies to all businesses
+      # @return [Array<Hashie::Mash>]
+      # @example
+      #   @client = SBA.new
+      #   @client.by_category("doing business as")
+      def by_category(category, options={})
+        get("/license_permit/by_category/#{category}.json", options)
+      end
+    
+      def by_state(state, options={})
+        get("/license_permit/all_by_state/#{state}.json", options)
+      end
     end
   end
 end
