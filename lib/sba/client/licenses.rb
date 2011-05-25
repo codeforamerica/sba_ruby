debce0966cdb116ed0c6e90372bdc23cf7dc79d8
T S <YenTheFirst@gmail.com>
2011-05-24 17:11:10 -0700

    licenses now pull and return correctly

--- lib/sba/client/licenses.rb
+++ lib/sba/client/licenses.rb
@@ -4,16 +4,28 @@ require 'json'
 module SBA
   class Client
     module Licenses
-    include SBA::Client::Connection
+    
+      include SBA::Client::Connection
+      
       def by_category(category, options={})
         options.merge!({:format=> "json"})
         response = get("/license_permit/by_category/#{category}.json")
+        simplify_response(response)
       end
     
       def by_state(state, options={})
         options.merge!({:format=> "json"})
-        resonse = get("/license_permit/all_by_state/#{state}.json", options)
+        response = get("/license_permit/all_by_state/#{state}.json", options)
+        simplify_response(response)
       end
+      
+      private
+      def simplify_response(response)
+        #the returned 'key' is pretty useless. the key=>value thing here is basically an array
+        #within that, we're given an array of key=>value pairs. we want this to be one big hash.
+        response.map {|key,value| value.inject({}) {|all,current| all.merge! current}}
+      end
+      
     end
   end
 end
