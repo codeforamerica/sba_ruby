4160ebd23f9c939fb93396dd4f540841c1574089
T S <YenTheFirst@gmail.com>
2011-05-24 18:09:46 -0700

    point spec at the actual location / url

--- spec/licenses_spec.rb
+++ spec/licenses_spec.rb
@@ -18,7 +18,7 @@ describe SBA do
     it "should return the correct results" do
       test = SBA.by_category('doing business as')
       test.should be_a Array
-      test[0]["url"].should == "http://www.sec.state.vt.us/tutor/dobiz/dobizdoc.htm"
+      test[52]["url"].should == "http://www.sec.state.vt.us/tutor/dobiz/dobizdoc.htm"
     end
   end
 
