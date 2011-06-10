3e7a5bd41eedd1d31fde00b98b19315d244418b4
Ryan Resella <ryan@codeforamerica.org>
2011-06-10 15:30:42 -0700

    Added specs for data_county

--- spec/data_spec.rb
+++ spec/data_spec.rb
@@ -41,4 +41,24 @@ describe SBA do
       test[1]["url"].should == "http://ci.agoura-hills.ca.us/"
     end
   end
+  describe ".data_county" do
+    before do
+      stub_request(:get, 'http://api.sba.gov/geodata/county_data_for_state_of/ca.json').
+        with().
+        to_return(:body => fixture('data_county.json'),
+                  :headers => {'Content-Type' => 'application/json'})
+    end
+    it "should request the correct resource" do
+      SBA.data_county('ca')
+      a_request(:get, 'http://api.sba.gov/geodata/county_data_for_state_of/ca.json').
+        with().
+        should have_been_made
+    end
+    it "should return the correct results" do
+      test = SBA.data_county('ca')
+      test.should be_an Array
+      test[0]["url"].should == "http://www.acgov.org/"
+      test[1]["url"].should == "http://www.alpinecountyca.gov/"
+    end
+  end
 end
