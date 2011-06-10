64478865bc619f808ecd523861724319bd460d39
Ryan Resella <ryan@codeforamerica.org>
2011-06-10 13:43:34 -0700

    Removed options.merge from each function

--- lib/sba/client/urls.rb
+++ lib/sba/client/urls.rb
@@ -6,24 +6,19 @@ module SBA
     module Urls
     include SBA::Client::Connection
       def all_primary_urls_in_state(state, options={})
-        options.merge!({:format=> "json"})
-        response = get("geodata/primary_city_county_links_for_state_of/#{state}.json")
+        response = get("geodata/primary_city_county_links_for_state_of/#{state}.json", options)
       end
       def all_city_primary_urls_in_state(state, options={})
-        options.merge!({:format => "json"})
-        response = get("geodata/primary_city_links_for_state_of/#{state}.json")
+        response = get("geodata/primary_city_links_for_state_of/#{state}.json", options)
       end
       def all_county_primary_urls_in_state(state, options={})
-        options.merge!({:format => "json"})
-        response = get("geodata/primary_county_links_for_state_of/#{state}.json")
+        response = get("geodata/primary_county_links_for_state_of/#{state}.json", options)
       end
       def primary_url_for_city(city, state, options={})
-        options.merge!({:format => "json"})
-        response = get("geodata/primary_links_for_city_of/#{city}/#{state}.json")
+        response = get("geodata/primary_links_for_city_of/#{city}/#{state}.json", options)
       end
       def all_primary_urls_for_county(county, state, options={})
-        options.merge!({:format => "json"})
-        response = get("geodata/primary_links_for_county_of/#{county}/#{state}.json")
+        response = get("geodata/primary_links_for_county_of/#{county}/#{state}.json", options)
       end
     end
   end
