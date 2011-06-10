e73907074dd93bc9f310d66863b6ccea45ce7cf5
Ryan Resella <ryan@codeforamerica.org>
2011-06-10 11:22:12 -0700

    Added spec for sites by keyword

--- spec/sites_spec.rb
+++ spec/sites_spec.rb
@@ -18,3 +18,22 @@ describe SBA, ".all_sites" do
     test["recommended_sites_item150"][0]["title"] == "Tax Help and Training"
   end
 end
+
+describe SBA, ".sites_by_keyword" do
+  before do
+    stub_request(:get, 'http://api.sba.gov/rec_sites/keywords/contracting.json').
+      to_return(:body => fixture('rec_sites_keywords.json'), :headers => {'Content-Type' => 'text/json; charset=utf-8'})
+  end
+
+  it "should request the correct resource" do
+    SBA.all_sites()
+    a_request(:get, 'http://api.sba.gov/rec_sites/keywords/contracting.json').
+    should have_been_made
+  end
+
+  it "should return the correct results" do
+    test = SBA.all_sites()
+    test.should be_a Hash
+    test["recommended_sites_item0"][0]["title"] == "How to Find Contracting Opportunities"
+  end
+end
