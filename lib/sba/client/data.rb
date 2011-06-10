bbe36eaa50bac7b8014b92e5edf5eec3fb7ccc80
Ryan Resella <ryan@codeforamerica.org>
2011-06-10 15:27:14 -0700

    Added documentation for data_city

--- lib/sba/client/data.rb
+++ lib/sba/client/data.rb
@@ -18,6 +18,14 @@ module SBA
         response = get("geodata/city_county_data_for_state_of/#{state}.json", options)
       end
       
+      # Returns Data for all Cities in a State
+      #
+      # @param state [String]  Input the two leterl postal code for state abbreviation.
+      # @param options [Hash] A customizable set of options.
+      # @return {Array}
+      # @see http://www.sba.gov/content/us-city-and-county-web-data-api-city-county-data-all-data-methods
+      # @example
+      #   SBA.data_city("ca")
       def data_city(state, options={})
         response = get("geodata/city_data_for_state_of/#{state}.json", options)
       end
