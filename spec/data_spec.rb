10e3148607d39090ce9c7b036a455654d2c60ec8
Ryan Resella <ryan@codeforamerica.org>
2011-06-10 15:24:33 -0700

    Added spec for data by city

--- spec/data_spec.rb
+++ spec/data_spec.rb
@@ -21,4 +21,24 @@ describe SBA do
       test[1]["url"].should == "http://ci.agoura-hills.ca.us/"
     end
   end
+  describe ".data_city" do
+    before do
+      stub_request(:get, 'http://api.sba.gov/geodata/city_data_for_state_of/ca.json').
+        with().
+        to_return(:body => fixture('data_city.json'),
+                  :headers => {'Content-Type' => 'application/json'})
+    end
+    it "should request the correct resource" do
+      SBA.data_city('ca')
+      a_request(:get, 'http://api.sba.gov/geodata/city_data_for_state_of/ca.json').
+        with().
+        should have_been_made
+    end
+    it "should return the correct results" do
+      test = SBA.data_city('ca')
+      test.should be_an Array
+      test[0]["url"].should == "http://www.ci.adelanto.ca.us/"
+      test[1]["url"].should == "http://ci.agoura-hills.ca.us/"
+    end
+  end
 end
