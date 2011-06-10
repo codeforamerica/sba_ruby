b2d73b2dc14d173d61028825e281fe7b8af2e5bf
Ryan Resella <ryan@codeforamerica.org>
2011-06-10 14:30:37 -0700

    Added spec for all_urls_city

--- spec/urls_spec.rb
+++ spec/urls_spec.rb
@@ -113,4 +113,22 @@ describe SBA do
       result[1]['url'].should == 'http://ci.agoura-hills.ca.us/'
     end
   end
+  describe ".all_urls_city" do
+    before do
+      stub_request(:get, 'http://api.sba.gov/geodata/city_links_for_state_of/ca.json').
+        with().
+        to_return(:body => fixture('url_city.json'))
+    end
+    it "should request the correct resource" do
+      SBA.all_urls_city('ca')
+      a_request(:get, 'http://api.sba.gov/geodata/city_links_for_state_of/ca.json').
+        with().
+        should have_been_made
+    end
+    it "should get the correct data" do
+      result = SBA.all_urls_city('ca')
+      result[0]['url'].should == 'http://www.ci.adelanto.ca.us/'
+      result[1]['url'].should == 'http://ci.agoura-hills.ca.us/'
+    end
+  end
 end
