913ca32edd2848fb355e0abc704cbab149c405e2
Ryan Resella <ryan@codeforamerica.org>
2011-06-10 15:40:52 -0700

    Added spec for search data by specific county

--- spec/data_spec.rb
+++ spec/data_spec.rb
@@ -80,4 +80,24 @@ describe SBA do
       test[0]["url"].should == "http://seattle.gov/"
     end
   end
+  describe ".data_specific_county" do
+    before do
+      stub_request(:get, 'http://api.sba.gov/geodata/all_data_for_county_of/frederick%20county/md.json').
+        with().
+        to_return(:body => fixture('data_specific_county.json'),
+                  :headers => {'Content-Type' => 'application/json'})
+    end
+    it "should request the correct resource" do
+      SBA.data_specific_county('frederick county', 'md')
+      a_request(:get, 'http://api.sba.gov/geodata/all_data_for_county_of/frederick%20county/md.json').
+        with().
+        should have_been_made
+    end
+    it "should return the correct results" do
+      test = SBA.data_specific_county('frederick county', 'md')
+      test.should be_an Array
+      test[0]["url"].should == "http://www.brunswickmd.gov/"
+      test[1]["url"].should == "http://www.emmitsburg.net/towngov/"
+    end
+  end
 end
